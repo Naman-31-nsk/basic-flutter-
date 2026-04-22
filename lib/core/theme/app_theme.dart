0804d1c47955d2e1e9b30c21c8d6572ef9cae821
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-22 17:16:30 +0530

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,16 @@
 
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const heading = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: AppColors.black,
+  );
+
+  static const normal = TextStyle(
+    fontSize: 16,
+    color: AppColors.black,
+  );
+}
