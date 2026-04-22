fd0085e078c64adfe937a046c1cf293486455bce
Naman-31-nsk <codebreakernaman@gmail.com>
2026-04-22 17:16:56 +0530

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,16 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'app_colors.dart';
 
-class AppTextStyles {
-  static const heading = TextStyle(
-    fontSize: 28,
-    fontWeight: FontWeight.bold,
-    color: AppColors.black,
-  );
-
-  static const normal = TextStyle(
-    fontSize: 16,
-    color: AppColors.black,
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.white,
+    useMaterial3: true,
   );
 }
